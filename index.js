d020172fec1d8b2b59f7a63eeb4f8d9898990e98
roseminted <57846775+roseminted@users.noreply.github.com>
2020-12-14 10:40:47 -0500

    add basic styles and JS

---
+++ index.js
@@ -0,0 +1,12 @@
+let place1 = prompt("Enter a place: ");
+let noun1 = prompt("Enter a noun: ");
+let verb1 = prompt("Enter a verb: ");
+let noun2 = prompt("Enter a noun: ");
+let pluralNoun = prompt("Enter a plural noun: ");
+let place2 = prompt("Enter a place: ");
+let verb2 = prompt("Enter a verb: ");
+
+
+let result = 'On my birthday, I went to ' + place1 + ' and had a ' + noun1 + ', it was so good i could ' + verb1 + '. Then i went home and opened my gifts, my sister gave me a ' + noun2 + ' and my brother gave me ' + pluralNoun + '. It was so much better than last year, that I had to go to ' + place2 + ' so that I could ' + verb2 + '.'
+
+alert(result);
