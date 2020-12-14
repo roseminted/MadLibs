8637e465133dcc858a5c550556f6bc52b1745b9a
roseminted <57846775+roseminted@users.noreply.github.com>
2020-12-14 11:01:42 -0500

    update JS

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # MadLibs
+    Fill in all of the input boxes, then click the "Mad Lib" button to generate a story.
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-let place1 = prompt("Enter a place: ");
-let noun1 = prompt("Enter a noun: ");
-let verb1 = prompt("Enter a verb: ");
-let noun2 = prompt("Enter a noun: ");
-let pluralNoun = prompt("Enter a plural noun: ");
-let place2 = prompt("Enter a place: ");
-let verb2 = prompt("Enter a verb: ");
+
+document.getElementById("button").addEventListener("click", buildMadLib);
+
+function buildMadLib() {
+    let place1 = document.getElementById("place1").value;
+    let noun1 = document.getElementById("noun1").value;
+    let verb1 = document.getElementById("verb1").value;
+    let noun2 = document.getElementById("noun2").value;
+    let pluralNoun = document.getElementById("pluralNoun").value;
+    let place2 = document.getElementById("place2").value;
+    let verb2 = document.getElementById("verb2").value;
+
+    let story = 'On my birthday, I went to ' + place1 + ' and had a ' + noun1 + ', it was so good i could ' + verb1 + '. Then i went home and opened my gifts, my sister gave me a ' + noun2 + ' and my brother gave me ' + pluralNoun + '. It was so much better than last year, that I had to go to ' + place2 + ' so that I could ' + verb2 + '.'
+
+    document.getElementById("story").innerHTML = story;
+}
 
 
-let result = 'On my birthday, I went to ' + place1 + ' and had a ' + noun1 + ', it was so good i could ' + verb1 + '. Then i went home and opened my gifts, my sister gave me a ' + noun2 + ' and my brother gave me ' + pluralNoun + '. It was so much better than last year, that I had to go to ' + place2 + ' so that I could ' + verb2 + '.'
-
-alert(result);
